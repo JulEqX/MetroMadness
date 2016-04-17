f883e8c72e5218b6b4e71db68de395ec2ba8d53d
JulEqX <jcwk94_eqx@hotmail.com>
2016-04-17 11:52:06 +1000

    PG change

--- Metro Madness/core/src/com/unimelb/swen30006/metromadness/passengers/PassengerGenerator.java
+++ Metro Madness/core/src/com/unimelb/swen30006/metromadness/passengers/PassengerGenerator.java
@@ -6,55 +6,39 @@ import com.unimelb.swen30006.metromadness.stations.ActiveStation;
 import com.unimelb.swen30006.metromadness.stations.Station;
 import com.unimelb.swen30006.metromadness.tracks.Line;
 
-/**
- * Generates a specified number of random passengers given a starting station
- *
- */
 public class PassengerGenerator {
-
-	/* The station that passengers are getting on */
+	
+	// The station that passengers are getting on
 	public Station start;
-
-	/* Maximum number of passengers to generate */
+	
+	// The max volume
 	public float maxVolume;
+	
+	private static int RANDOM = 10;
 
-	/* Randomise number of line travels */
-	private static final int RANDOM = 10;
-
-	public PassengerGenerator(Station s, float max) {
+	
+	public PassengerGenerator(Station s, float max){
 		this.start = s;
 		this.maxVolume = max;
 	}
-
-	/**
-	 * Generates a random number of passengers that is less than or equal to the
-	 * max volume
-	 * 
-	 * @return an arraylist of passengers
-	 */
-	public Passenger[] generatePassengers() {
-		int count = (int) (Math.random() * maxVolume);
+	
+	public Passenger[] generatePassengers(){
+		int count = (int) (Math.random()*maxVolume);
 		Passenger[] passengers = new Passenger[count];
-		for (int i = 0; i < count; i++) {
+		for(int i=0; i<count; i++){
 			passengers[i] = generatePassenger();
 		}
 		return passengers;
 	}
-
-	/**
-	 * Generates a single passenger
-	 * 
-	 * @return passenger class
-	 */
-	public Passenger generatePassenger() {
-		// Chooses random number of lines to travel on
-		int numLineTravels = (int) (Math.random() * (RANDOM)) + 1;
-
-		// Lists that hold what stations and lines the passenger travels to
+	
+	public Passenger generatePassenger(){
+		//Chooses random number of lines to travel on
+		int numLineTravels = (int) (Math.random()*(RANDOM))+1;
+		
+		//Lists that hold what stations and lines the passenger travels to
 		ArrayList<Station> travelStations = new ArrayList<Station>();
-
+		
 		Station temporary = start;
-<<<<<<< HEAD
 		for(int i = 0; i < numLineTravels; i++) {
 			
 			//Get a random line from current position
@@ -70,30 +54,17 @@ public class PassengerGenerator {
 				newStation = newLine.getStations().get((int)(Math.random()*newLine.getStations().size()));
 				System.out.println("Loops");
 				cls = newStation.getClass();
-=======
-		for (int i = 0; i < numLineTravels; i++) {
-
-			// Get a random line
-			Line newLine = temporary.getLines().get((int) (Math.random() * temporary.getLines().size()));
-
-			// Get a random station on that line
-			Station newStation = newLine.getStations().get((int) (Math.random() * newLine.getStations().size()));
-
-			// Make sure that station is an active station
-			while (!(start.getClass().toString().equals(newStation.getClass().toString()))) {
-				newStation = newLine.getStations().get((int) (Math.random() * newLine.getStations().size()));
->>>>>>> origin/master
 			}
-
-			// Add new station to travel list
+			
+			//Add new station to travel list
 			travelStations.add(newStation);
 			temporary = newStation;
 
 		}
-		// Destination is the last station on the travel list
-		Station destination = travelStations.get(travelStations.size() - 1);
+		//Destination is a station on the last travel line
+		Station destination = travelStations.get(travelStations.size()-1);
 		travelStations.remove(destination);
 		return new Passenger(start, travelStations, destination);
 	}
-
+	
 }
